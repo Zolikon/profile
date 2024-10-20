a4da86574c1a63e64baf91328cc202a570c2f310
Zoltan Pozsonyi <pozsonyizoli@gmail.com>
2024-10-20 20:51:47 +0200

    add question suggestion for chat

--- src/chat/RobotMe.jsx
+++ src/chat/RobotMe.jsx
@@ -19,10 +19,18 @@ function RobotMe() {
     }
   }
 
+  function submitSuggestions(question) {
+    if (question.length > 0 && !waitingForAnswer) {
+      askQuestion(question);
+      setCurrentMessage("");
+      setDisplayInput(false);
+    }
+  }
+
   return (
     <div className="flex flex-col justify-start gap-4 items-center text-xs w-full h-full xl:text-sm py-6 pr-10 xl:pr-0">
       <p className="text-6xl font-extrabold">RobotMe</p>
-      <div className="flex flex-col justify-end h-3/5 overflow-hidden w-full xl:w-2/3  mt-10 px-4 gap-2 custom-scrollbar">
+      <div className="flex flex-col justify-end h-3/5 overflow-hidden w-full xl:w-2/3  mt-10 pl-2 xl:px-4  gap-2 custom-scrollbar">
         <AnimatePresence>
           {allMessages.map((message, index) => (
             <ChatMessage
@@ -40,7 +48,18 @@ function RobotMe() {
       </div>
       <AnimatePresence>
         {displayInput && (
-          <>
+          <div className="w-full flex flex-col items-center justify-center">
+            {allMessages.length === 0 && (
+              <div className="flex pb-4 gap-4 items-center justify-start w-4/5">
+                <QuestionSuggestion key="q1" question="Tell me about yourself" onClick={submitSuggestions} />
+                <QuestionSuggestion
+                  key="q2"
+                  question="We need a Java team lead, are you a good candidate?"
+                  onClick={submitSuggestions}
+                />
+                <QuestionSuggestion key="q3" question="How can I reach you?" onClick={submitSuggestions} />
+              </div>
+            )}
             <motion.div
               className="w-4/5 relative"
               initial={{ opacity: 0 }}
@@ -49,10 +68,12 @@ function RobotMe() {
               transition={{ duration: 0.5 }}
             >
               <textarea
+                placeholder="Ask me anything"
+                rows={3}
                 data-gramm="false"
                 value={currentMessage}
                 onChange={(e) => setCurrentMessage(e.target.value)}
-                className="p-2 rounded-xl w-full bg-light-bg-from dark:bg-dark-bg-from dark:text-dark-text pr-[50px] resize-none"
+                className="p-2 rounded-xl w-full bg-light-bg-from dark:bg-dark-bg-from dark:text-dark-text placeholder:dark:text-stone-500 placeholder:text-slate-500 pr-[50px] resize-none"
                 onKeyDown={(e) => {
                   if (e.key === "Enter") {
                     e.preventDefault();
@@ -70,7 +91,7 @@ function RobotMe() {
                 </button>
               )}
             </motion.div>
-          </>
+          </div>
         )}
         {displayInput && allMessages.length > 2 && currentMessage.length === 0 && (
           <motion.p
@@ -79,13 +100,12 @@ function RobotMe() {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
           >
-            Keep in mind that I might give smart answers but nothing replaces personal contact. You can find contact
-            information on the side menu.
+            {"I'm mainly act as a proof of AI experties. Want to know more? Find contacts in the menu."}
           </motion.p>
         )}
         {waitingForAnswer && (
           <div className="flex flex-col w-[120px]">
-            <Comment className="mt-2 self-start " backgroundColor="green" color="white" />
+            <Comment className="mt-2 self-start " backgroundColor="teal" color="white" />
             <p className="text-5xl self-end">🤖</p>
           </div>
         )}
@@ -93,6 +113,22 @@ function RobotMe() {
     </div>
   );
 }
+
+function QuestionSuggestion({ question, onClick }) {
+  return (
+    <button
+      onClick={() => onClick(question)}
+      className="bg-light-side-from dark:bg-dark-side-from p-2 rounded-xl text-left text-dark-text hover:scale-105 transition-all"
+    >
+      {question}
+    </button>
+  );
+}
+
+QuestionSuggestion.propTypes = {
+  question: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 function ChatMessage({ isHuman = false, message, links = [], typeMessage = false, onFinishTyping = () => {} }) {
   const [displayedMessage, setDisplayedMessage] = useState(typeMessage ? "" : message);
@@ -126,7 +162,7 @@ function ChatMessage({ isHuman = false, message, links = [], typeMessage = false
       }`}
     >
       <div className="flex">
-        <div className="text-3xl self-center">{isHuman ? "🧑‍💻" : "🤖"}</div>
+        <div className="text-3xl self-center select-none">{isHuman ? "🧑‍💻" : "🤖"}</div>
         <div className=" break-words">{displayedMessage}</div>
       </div>
       {displayedMessage.length === message.length && links && links.length > 0 && (
